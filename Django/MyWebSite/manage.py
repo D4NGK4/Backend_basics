53bd44b6c8e01bed3b781ad9b199d4594cd80a1f
D4NGK4 <danjosheps.djt98@gmail.com>
2023-08-22 01:38:15 +0800

    added todos in views and url

--- Django/MyWebSite/manage.py
+++ Django/MyWebSite/manage.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+#user: Dan
+#pass: 12345
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
--- Django/MyWebSite/mysite/views.py
+++ Django/MyWebSite/mysite/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, HttpResponse
+from .models import TodoItem
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
+def todos(request):
+    item = TodoItem.objects.all()
+    return render(request, "todos.html", {"todos": item})
