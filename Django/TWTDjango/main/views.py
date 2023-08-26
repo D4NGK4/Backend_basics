38ac56521cc00261b064177b4e74483d003cf26e
D4NGK4 <danjosheps.djt98@gmail.com>
2023-08-26 22:43:57 +0800

    added a new view

--- Django/TWTDjango/main/views.py
+++ Django/TWTDjango/main/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 def index(response):
     return HttpResponse("Hello World")
 
+def v1(response):
+    return HttpResponse("Hello World")
+
 # Create your views here.
